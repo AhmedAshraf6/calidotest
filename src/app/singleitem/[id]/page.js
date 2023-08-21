8bc28f6a69aaa0fd8ebc80295865fdd3962ba607
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-08-21 16:43:14 +0200

    edit in single item

--- src/app/singleitem/[id]/page.js
+++ src/app/singleitem/[id]/page.js
@@ -4,8 +4,15 @@ import LinkComponent from '@/components/singleitem/LinkComponent';
 import Slider from '@/components/singleitem/Slider';
 import Link from 'next/link';
 import React from 'react';
-
-export default function page() {
+import { BaseUrl } from '@/util/constants';
+async function getProduct(productID) {
+  const res = await fetch(`${BaseUrl}/products/${productID}`);
+  return await res.json();
+}
+export default async function page({ params }) {
+  const productId = params.id;
+  const { product } = await getProduct(productId);
+  console.log(product);
   const images = [
     'https://images.pexels.com/photos/132340/pexels-photo-132340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     'https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -18,20 +25,21 @@ export default function page() {
       <div className='container'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4 '>
           <Slider images={images} />
-          <Info />
+          <Info product={product} />{' '}
         </div>
         <div className='flex justify-center gap-4 border-b-2 border-gray-200 mt-8 sm:mt-24'>
-          <LinkComponent href={`/singleitem/asd`}> Description</LinkComponent>
-          <LinkComponent href={`/reviewitem/asd`}> Review(0)</LinkComponent>
+          <LinkComponent href={`/singleitem/${product?.id}`}>
+            {' '}
+            Description
+          </LinkComponent>
+          <LinkComponent href={`/reviewitem/${product?.id}`}>
+            {' '}
+            Review(0)
+          </LinkComponent>
         </div>
         <div className='mt-4 sm:mt-8'>
           <h2 className='text-base sm:text-lg font-bold'>Descripton</h2>
-          <p className='paragraph mt-3 sm:mt-5'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            quasi voluptatibus ex vitae reprehenderit ipsam quo, aperiam quod
-            debitis iusto, tenetur non sint dignissimos voluptates aut! Possimus
-            at ut eveniet?
-          </p>
+          <p className='paragraph mt-3 sm:mt-5'>{product?.description}</p>
         </div>
         <div className='mt-4 sm:mt-12'>
           <h1 className='text-lg sm:text-xl font-bold mb-3 sm:mb-4'>
